bd4479515b341c62efe3b89ae660307ffd4298c6
haiduk <hwlee91@gmail.com>
2020-07-11 03:18:51 -0400

    First commit

---
+++ tictactoe.rb
@@ -0,0 +1,24 @@
+puts "Welcome to tic-tac-toe! Each player will take 
+turns placing an X or an O. The winner is the 
+player with three in a row."
+
+class Squares
+  def initialize
+  end
+end
+
+puts "| #{1} | #{2} | #{3} |
+| #{4} | #{5} | #{6} |
+| #{7} | #{8} | #{9} |"
+
+=begin
+introduction of rules
+has 9 slots to place
+each player takes turns placing
+if a player hits 3 in a row, they win
+if all spots are filled without a winner, it is a draw
+
+each of the 9 squares will be a class
+then will need to differentiate x and o
+then input win/draw conditions
+=end
