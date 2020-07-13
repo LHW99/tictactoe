8d5ee67f77e8cc6e6b48994e25b2c6c7ab316d77
haiduk <hwlee91@gmail.com>
2020-07-13 16:09:03 -0400

    Trying to get square to change from input

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,15 +1,37 @@
-puts "Welcome to tic-tac-toe! Each player will take 
-turns placing an X or an O. The winner is the 
-player with three in a row."
-
-class Squares
+class TicTacToe
   def initialize
+    introScreen
   end
-end
 
-puts "| #{1} | #{2} | #{3} |
+  def board
+    puts  "| #{1} | #{2} | #{3} |
 | #{4} | #{5} | #{6} |
 | #{7} | #{8} | #{9} |"
+  end
+
+  def introScreen
+    puts  "Welcome to tic-tac-toe! Each player will take 
+turns placing an X or an O. The winner is the 
+player with three X's or O's in a row.
+    "
+    puts board
+  end
+=begin
+  case gets.strip
+    when "1"
+    when "2"
+    when "3"
+    when "4"
+    when "5"
+    when "6"
+    when "7"
+    when "8"
+    when "9"
+  end
+=end
+end
+
+TicTacToe.new
 
 =begin
 introduction of rules
@@ -18,7 +40,5 @@ each player takes turns placing
 if a player hits 3 in a row, they win
 if all spots are filled without a winner, it is a draw
 
-each of the 9 squares will be a class
-then will need to differentiate x and o
-then input win/draw conditions
+step 1: turn a square into an x
 =end
