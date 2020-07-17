3ec7e545ec7197995edd84da8cf7a6e71a7f0a80
haiduk <hwlee91@gmail.com>
2020-07-17 02:23:31 -0400

    switches between x and o every turn

--- tictactoe.rb
+++ tictactoe.rb
@@ -4,8 +4,6 @@ module TicEquations
     end
   end
 
-
-
 class TicTacToe
   include TicEquations
   @@sq1 = 1; @@sq2 = 2; @@sq3 = 3; @@sq4 = 4;
@@ -13,6 +11,8 @@ class TicTacToe
   @@sq9 = 9
   @@turn = 0
   @@selections = []
+  @@selectionsO = []
+
   def initialize
     introScreen
   end
@@ -30,63 +30,63 @@ class TicTacToe
       when "1"
         @@turn += 1
         @@selections.push(1)
-        @@sq1 = 'x'
+        @@turn % 2 == 0 ? @@sq1 = 'x' : @@sq1 = 'o'
         puts winCondition
         puts board
         puts selections
       when "2"
         @@turn += 1
         @@selections.push(2)
-        @@sq2 = 'x'
+        @@turn % 2 == 0 ? @@sq2 = 'x' : @@sq2 = 'o'
         puts winCondition
         puts board
         puts selections
       when "3"
         @@turn += 1
         @@selections.push(3)
-        @@sq3 = 'x'
+        @@turn % 2 == 0 ? @@sq3 = 'x' : @@sq3 = 'o'
         puts winCondition
         puts board
         puts selections
       when "4"
         @@turn += 1
         @@selections.push(4)
-        @@sq4 = 'x'
+        @@turn % 2 == 0 ? @@sq4 = 'x' : @@sq4 = 'o'
         puts winCondition
         puts board
         puts selections
       when "5"
         @@turn += 1
         @@selections.push(5)
-        @@sq5 = 'x'
+        @@turn % 2 == 0 ? @@sq5 = 'x' : @@sq5 = 'o'
         puts winCondition
         puts board
         puts selections
       when "6"
         @@turn += 1
         @@selections.push(6)
-        @@sq6 = 'x'
+        @@turn % 2 == 0 ? @@sq6 = 'x' : @@sq6 = 'o'
         puts winCondition
         puts board
         puts selections
       when "7"
         @@turn += 1
         @@selections.push(7)
-        @@sq7 = 'x'
+        @@turn % 2 == 0 ? @@sq7 = 'x' : @@sq7 = 'o'
         puts winCondition
         puts board
         puts selections
       when "8"
         @@turn += 1
         @@selections.push(8)
-        @@sq8 = 'x'
+        @@turn % 2 == 0 ? @@sq8 = 'x' : @@sq8 = 'o'
         puts winCondition
         puts board
         puts selections
       when "9"
         @@turn += 1
         @@selections.push(9)
-        @@sq9 = 'x'
+        @@turn % 2 == 0 ? @@sq9 = 'x' : @@sq9 = 'o'
         puts winCondition
         puts board
         puts selections
@@ -105,21 +105,21 @@ player with three X's or O's in a row. \n\n"
 
   def winCondition
     if checker(@@selections, [1,2,3]) == true
-      puts "\nWINNER!!"
+      puts "\nX IS THE WINNER!!"
     elsif checker(@@selections, [4,5,6]) == true
-      puts "\nWINNER!!"
+      puts "\nX IS THE WINNER!!"
     elsif checker(@@selections, [7,8,9]) == true
-      puts "\nWINNER!!"
+      puts "\nX IS THE WINNER!!"
     elsif checker(@@selections, [1,4,7]) == true
-      puts "\nWINNER!!"
+      puts "\nX IS THE WINNER!!"
     elsif checker(@@selections, [2,5,8]) == true
-      puts "\nWINNER!!"
+      puts "\nX IS THE WINNER!!"
     elsif checker(@@selections, [3,6,9]) == true
-      puts "\nWINNER!!"
+      puts "\nX IS THE WINNER!!"
     elsif checker(@@selections, [1,5,9]) == true
-      puts "\nWINNER!!"
+      puts "\nX IS THE WINNER!!"
     elsif checker(@@selections, [3,5,7]) == true
-      puts "\nWINNER!!"
+      puts "\nX IS THE WINNER!!"
     else ""
     end
   end
